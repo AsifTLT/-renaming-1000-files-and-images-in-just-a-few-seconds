3743a70867474e59f676db9455147ef0c8e488d6
Mohammad Asif <115681231+AsifTLT@users.noreply.github.com>
2024-05-19 00:08:07 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import os
 files = os.listdir("images")
 i = 1
 for file in files:
-    if file.endswith(".png"): #jeisob file ends with .png oisob file k print koro
-        print(file)    # print all files and images
-        
+    if file.endswith(".png"): 
+        print(file)   
     os.rename(f"images/{file}", f"images/{i}.png")
-    i = i + 1 # sokol png file er nam change hoyeche serial by 1-17    
+    i = i + 1 
         
 
-    os.rename("images/chatsheet.pdf", "images/importantsheet.pdf") # chatsheet.pdf file name change to importantsheet.pdf just use simply os module
-    # os.rename("images/text.txt", "images/documents.txt") # text.txt file name change to documents.txt just use simply os module
+    os.rename("images/chatsheet.pdf", "images/importantsheet.pdf") 
+    # os.rename("images/text.txt", "images/documents.txt") 
 
